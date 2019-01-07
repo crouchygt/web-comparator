77bf980ed62a81d9dc2f7c429f9d19b36287672c
Matthew Crouch <matt@crazypanda.co.uk>
2019-01-07 07:30:24 +0000

    More verbose output

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const compare = require('./src/compare');
 const url1 = process.argv[2];
 const url2 = process.argv[3];
 
+console.log(`Capturing: ${url1}`);
 capture(url1).then(image1 => {
+    console.log(`Capturing: ${url2}`);
     capture(url2).then(image2 => {
         console.log('Starting Comparison');
         compare([
